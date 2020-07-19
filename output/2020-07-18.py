dcb1e100b8455e77697d412c53e0e82717f14261
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-18 22:05:28 -0300

    update live code

--- output/2020-07-18.py
+++ output/2020-07-18.py
@@ -1,20 +1,13 @@
 f"{print('hello')}"
-f'{print('hello')}
 f"'{print('hello')}
+f'{print('hello')}
 f"{print('hello')}"
+"'
+"'
 ''''
 """
 '"'"
-"'
-"'
-2+2'2+2"2+2'2+2"2+2
-
-for i in range(10):
-  print(i)
-
-for i in range(10):
-  print("Ola mundo\n/nTeste", i)
-
-print('Hello world')
-
- 
+zzz
+2+2'2+2"2+2'2+2"2+2for i in range(10):
+  print(i)for i in range(10):
+  print("Ola mundo\n/nTeste", i)print('Hello world') 
