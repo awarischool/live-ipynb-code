97c9590fb5911f77710e83ec1c56d18085c919fb
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 19:31:41 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -15,4 +15,6 @@
 import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/WittmannF/awari-calculadora-imoveis-may-20/master/1-web-scraping/dados_calculadora_imoveis_aula.csv
 
- 
+df.head()
+
+df
