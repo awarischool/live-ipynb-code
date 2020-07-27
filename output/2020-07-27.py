8fb94a1c2ac098c21011c5a01f7eac7f52977e37
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 18:51:21 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -8,7 +8,8 @@
 
 1. Abrir planilha da última aula
  2. Qual é o nosso objetivo final?
-3. Qual seria o melhor preço para as se
+3. Qual seria o melhor preço para as seguites imóveis (coletados da 
+primeira página do OLS
  
 
  
