19bc18ffbd47ea689b4d2bcd842204df39d794ea
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 19:27:01 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -10,7 +10,14 @@
 
 
 
-
+### 3. Qual seria o melhor preço para as seguites imóveis (coletados da 
+ primeira página do OLX)?
+- Leste, 2 quartos, 100m2
+- Leste, 2 quartos, 43m2
+- Sul, 1 quarto, 41m2
+- Sul, 1 quarto, 22m2
+ ####  3. Qual seria o melhor preço para as seguites imóveis (coletados da 
+primeira página do OLX)?
 
  
 
