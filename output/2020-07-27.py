e4292176ca5262fb1c66fc337c184f2e3b1102b7
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 18:59:23 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -4,18 +4,9 @@
 
 
 
+## 2. Plano de Aula
 
 
-1. Abrir planilha da última aula
- 2. Qual é o nosso objetivo final?
-3. Qual seria o melhor preço para as seguites imóveis (coletados da 
-primeira página do OLX)?
-    - a
-    - b
-    - c
-4. Brainstorming de estimativa de preço a partir da área
-5. Implementar algumas ideias 
- 
 
  
 
