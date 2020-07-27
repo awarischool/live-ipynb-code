008d3432a02975914a5c34e29d3a41aec9f503ad
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 18:54:15 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -6,14 +6,10 @@
 
 
 
-1. Abrir planilha da última aula
- 2. Qual é o nosso objetivo final?
+ 1. Abrir planilha da última aula
+2. Qual é o nosso objetivo final?
 3. Qual seria o melhor preço para as seguites imóveis (coletados da 
 primeira página do OLX):
-    - Apartamento para Alugar ao lado do Metro Chácara Klabin, 1 quarto | 
-41m² | 1 vaga, São Paulo, Vila Mariana - DDD 11
-    - Apartamento com 2 dormitórios para alugar, 55 m² - conjunto 
-residencial pombeva - são bern,  quartos | 55m² |
     - 
  
 
