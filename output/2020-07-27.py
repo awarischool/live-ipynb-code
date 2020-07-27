555a33275e0d01550ce3f2463f9b8e5ef0530dbe
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 19:00:58 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -6,20 +6,7 @@
 
 
 
-1. Abrir planilha da última aula
- 2. Qual é o nosso objetivo final?
-3. Qual seria o melhor preço para as seguites imóveis (coletados da 
-primeira página do OLX)?
-    - a
-    - b
-    - c
-4. Brainstorming de estimativa de preço a partir da área
-5. Implementar algumas ideias "na mão"
-6. Sklearn Map e Replicação em Python
-7. Alguns Problemas
-8. Train test split
-9. Outlier, Onehot e FillN
- 
+   
 
  
 
