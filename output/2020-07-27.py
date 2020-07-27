fed23081f74b67b235bef38a059adbd4ec83b833
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 20:11:49 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -10,7 +10,28 @@
 
 
 
-
+ ### 3. Qual seria o melhor preço para as seguites imóveis (coletados da 
+primeira página do OLX)?
+- Leste, 2 quartos, 100m2
+- Leste, 2 quartos, 43m2
+- Sul, 1 quarto, 41m2
+- Sul, 1 quarto, 22m2
+ #### Brainstorming
+- Começar com variável que mais afeta
+- Média dos aps com 2 quartos na zona leste
+- Categorizar áreas
+   - Definir intervalos
+- Definir uma equação que correlacione area com aluguel
+$$aluguel = area * w + b$$
+ - Extendendo a equação 
+$$aluguel = area * w_1 + quartos * w_2 + b$$
+ #### Problemas
+- Como tratar a coluna zonas
+   - S1: Associar um número inteiro para cada zona (1 - Norte, 2 - Sul, 3 
+- Leste, 4 - Oeste)
+       - P: Não há proporcionalidade entre zonas
+   - Associar uma nova "variável" para cada zona
+ 
 
 import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/WittmannF/awari-calculadora-imoveis-may-20/master/1-web-scraping/dados_calculadora_imoveis_aula.csv
