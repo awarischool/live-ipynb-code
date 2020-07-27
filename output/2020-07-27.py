56c499200059f34288c048551ba2c13bad814d0c
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 20:54:19 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -41,6 +41,21 @@ y.head()
 X = df_onehot.drop('preco', axis=1)
 X.head()
 
+X.
+add
+add_prefix
+add_suffix
+agg
+aggregate
+align
+all
+any
+append
+apply
+applymap
+abs
+X.abs() -> ~FrameOrSeries
+
 # Regressão Linear
 # 𝑎𝑙𝑢𝑔𝑢𝑒𝑙=𝑎𝑟𝑒𝑎∗𝑤1+𝑞𝑢𝑎𝑟𝑡𝑜𝑠∗𝑤2+𝑧𝑙𝑒𝑠𝑡𝑒∗𝑤3+𝑧𝑛𝑜𝑟𝑡𝑒∗𝑤4+𝑧𝑜𝑒𝑠𝑡𝑒∗𝑤5+𝑧𝑠𝑢𝑙∗𝑤6+𝑏
 from sklearn.linear_model import LinearRegression
