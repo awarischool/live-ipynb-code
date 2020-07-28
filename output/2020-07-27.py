8aeb4ef8825bd7e1a53dbb3ad212b29822638073
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 21:03:32 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -61,7 +61,9 @@ reg.predict([[2, 100, 1, 0, 0, 0], [1, 41, 0, 0, 0, 1]])
 
 # Árvore de Decisão
 # Definir sequencia de regras para "chutar" o preço
- 
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+cross_val_score(regressor, X, y, cv=10)
 
 # k Nearest Neighbors
 # k vizinhos mais próximos
