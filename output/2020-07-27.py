3bc4be2b3ee92912e2c18228d824581d25062c8c
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 21:06:38 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -77,4 +77,6 @@ neigh.fit(X, y)
 
 # Prever para Leste, 2 quartos, 100m2
 # E para Sul, 1 quarto, 41m2
-reg_dt.predict([[2, 100, 1, 0, 0, 0], [1, 41, 0, 0, 0, 1]])
+neigh.predict([[2, 100, 1, 0, 0, 0], [1, 41, 0, 0, 0, 1]])
+
+ 
