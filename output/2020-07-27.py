22ba7e88ba429e377a066bdae5bce657b887de16
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 21:20:26 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -87,4 +87,8 @@ y_true = [1680, 3590]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=2)
 
-X_train.
+len(X_train)
+
+len(X_test)
+len(obj)
+Return the number of items in a container.
