60bfc2003ac3e195b07ffaa9768b2100a58cf9a7
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-07-27 21:01:22 -0300

    update live code

--- output/2020-07-27.py
+++ output/2020-07-27.py
@@ -56,7 +56,7 @@ reg.intercept_
 reg.coef_
 
 # Prever para Leste, 2 quartos, 100m2
-reg.predict([[2, 100, ], []])
+reg.predict([[2, 100, 1, 0, 0, 0], []])
 
 # Árvore de Decisão
 # Definir sequencia de regras para "chutar" o preço
