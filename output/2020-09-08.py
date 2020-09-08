dbcd51d578d7828867cba8cdd684ab3fe0ecef05
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-09-08 19:33:08 -0300

    update live code

--- output/2020-09-08.py
+++ output/2020-09-08.py
@@ -17,8 +17,5 @@ df[df['job_simp']=='na'].head()
 df.groupby('job_simp').mean()['avg_salary']
 
 df.groupby('job_state').mean()['avg_salary']
-DataFrame: df
-View
-DataFrame with shape (742, 32)
 
  
