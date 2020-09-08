743335604e2bccdda94b77f311df94e50bf34779
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-09-08 19:39:53 -0300

    update live code

--- output/2020-09-08.py
+++ output/2020-09-08.py
@@ -22,6 +22,10 @@ job_state_avg_salary.sort_values(ascending=False).head()
 
 import seaborn as sns
 
-sns.barplot(data=df.groupby('job_simp').mean()['avg_salary'])
+sns.barplot(data=df.groupby('job_simp').mean()['avg_salary'], )
+sns.barplot(x=None, y=None, hue=None, data=None, order=None, hue_order=None, estimator=<function mean at 0x7fc2d482f840>, ci=95, n_boot=1000, units=None, seed=None, orient=None, color=None, palette=None, saturation=0.75, errcolor='.26', errwidth=None, capsize=None, dodge=True, ax=None, **kwargs)
+Show point estimates and confidence intervals as rectangular bars.
+A bar plot represents an estimate of central tendency for a numeric variable with the height of each rectangle and provides some indication of the uncertainty around that estimate using error bars. Bar plots include 0 in the quantitative axis range, and they are a good choice when 0 is a meaningful value for the quantitative variable, and you want to make comparisons against it.
+For datasets where 0 is not a meaningful value, a point plot will allow you to focus on differences between levels of one or more categorical variables.
 
  
