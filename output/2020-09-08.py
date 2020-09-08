1d4c9c9eef0a8d4abc134c5c96fbe21bd843e18e
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-09-08 19:53:06 -0300

    update live code

--- output/2020-09-08.py
+++ output/2020-09-08.py
@@ -39,6 +39,13 @@ df['avg_salary'].hist()
 
 df.head()
 
+df.des()
+desc_len
+describe
+df.describe(percentiles=None, include=None, exclude=None) -> ~FrameOrSeries
+dtypes
+drop_duplicates
+
 num_cols = []
 sns.pairplot(df[num_cols])
 
