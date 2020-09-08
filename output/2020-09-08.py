10e0cceedffea1f73712571153a2603387e351ef
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-09-08 19:10:51 -0300

    update live code

--- output/2020-09-08.py
+++ output/2020-09-08.py
@@ -1,5 +1,5 @@
 import pandas as pd
-'https://github.com/PlayingNumbers/ds_salary_proj/raw/master/eda_data.csv'
-pandas as pd⏎
+url = 'https://github.com/PlayingNumbers/ds_salary_proj/raw/master/eda_data.csv'
+pd.read_csv()
 
  
